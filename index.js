9a881d27f4b978a1c06969a0601c697945052f98
hhshhhskk <hhshhhskk@naver.com>
2023-11-11 20:23:44 +0900

    swagger 설치

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import { options } from "./swagger/config.js";
+
 const app = express();
 const port = 4000;
+
+// swagger 설정
+const swaggerSpec = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(
   cors({
@@ -11,32 +19,53 @@ app.use(
   })
 );
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
   res.send("홈페이지 입니다.");
 });
 
 // 게시판 API
-app.get("/board", (요청, 반응) => {
+app.get("/board", (req, res) => {
+  console.log("GET", req);
+  // DB 에서 조건을 보고 결과를 {} 만들어서 [] 담아서준다.
+  // MongoDB, MaraiDB(MySql)
   const result = [
     {
-      pk: 1,
-      id: 1,
-      user: "둘리",
-      title: "반가워요",
-      content: "내용입니다.",
+      number: 1,
+      writer: "철수",
+      title: "제목입니다.",
+      contents: "내용입니다.",
+    },
+    {
+      number: 2,
+      writer: "영희",
+      title: "영희입니다.",
+      contents: "내용입니다.",
+    },
+    {
+      number: 3,
+      writer: "훈희",
+      title: "훈희입니다.",
+      contents: "내용입니다.",
     },
   ];
-  반응.send(result);
+  res.send(result);
 });
 
-app.get("/good", (요청, 반응) => {
-  // DB 가서 ...... 목록 출력.. 노력..
-  const result = [{ title: "사과", price: 1000, discount: 25 }];
-  반응.send(result);
+// post 는 프론트에서 백엔드로 자료 전송
+// localhost:4000/board : 게시판 자료를 추가한다.
+// axios.post("/board", {자료})
+app.post("/board", (req, res) => {
+  console.log("POST", req);
+  console.log("BODY 프론트가 보낸 데이터 ", req.body);
+  // DB 에 내용 추가
+  res.send("게시물 추가에 성공하였습니다.");
 });
 
 // visual 영역 출력 데이터
-app.get("/visual", (요청, 반응) => {
+app.get("/visual", (req, res) => {
   const result = {
     total: 6,
     visual_slide: [
@@ -66,9 +95,192 @@ app.get("/visual", (요청, 반응) => {
       },
     ],
   };
-  반응.send(result);
+  res.send(result);
 });
-
+// recommend 영역 출력 데이터
+app.get("/recommend", (req, res) => {
+  const result = {
+    total: 11,
+    total_img: {
+      file: "images/total.svg",
+      url: "a.html",
+    },
+    recommend_slide: [
+      {
+        file: "images/recommend/recommend1.jpeg",
+        prodName:
+          "[무료배송] GAP/네파키즈 유아 아동양말 특가전 6팩 건조기가능/KC인증",
+        discount: 50,
+        price: 6900,
+        url: "a.html",
+      },
+      {
+        file: "images/recommend/recommend2.jpeg",
+        prodName: "[쎈딜] 서울우유 멸균 초코/딸기/흰우유 24팩/48팩 외",
+        discount: 38,
+        price: 12500,
+        url: "b.html",
+      },
+      {
+        file: "images/recommend/recommend3.jpeg",
+        prodName: "[쎈딜] 아모스 컬링 에센스 2X 150ML 2개",
+        discount: 12,
+        price: 24120,
+        url: "c.html",
+      },
+      {
+        file: "images/recommend/recommend4.jpeg",
+        prodName:
+          "프로쉬 독일 식세기 세제 30개입 2개 올인원 식기세척기 전용세제 600g",
+        discount: 49,
+        price: 21130,
+        url: "d.html",
+      },
+      {
+        file: "images/recommend/recommend5.jpeg",
+        prodName: "[스포츠파크] EXR 여성 보아 후리스 자켓 롱자켓 12종택1",
+        discount: 66,
+        price: 12900,
+        url: "e.html",
+      },
+      {
+        file: "images/recommend/recommend6.jpeg",
+        prodName:
+          "[만원의행복] 호두앤 토핑호두과자 오리지널 40알 외 앙버터/누텔라/블루베리 혼합 골라담기",
+        discount: 0,
+        price: 8730,
+        url: "f.html",
+      },
+      {
+        file: "images/recommend/recommend7.png",
+        prodName:
+          "[쎈딜] [I*POP] 아이팝 스파클링 워터 플레인 / 아이팝 탄산수 / 강탄산 / 진짜 먹는 샘물로 만든 탄산수",
+        discount: 22,
+        price: 6900,
+        url: "g.html",
+      },
+      {
+        file: "images/recommend/recommend8.jpeg",
+        prodName:
+          "[쎈딜][스포츠파크] 노스페이스 컴피 알파 플리스 집업 NJ4FP57 JKL ALL",
+        discount: 26,
+        price: 79000,
+        url: "h.html",
+      },
+      {
+        file: "images/recommend/recommend9.png",
+        prodName:
+          "[쎈딜] 야나두키즈 12개월 통합이용권+영어전집 (+30일 추가증정)",
+        discount: 88,
+        price: 160550,
+        url: "i.html",
+      },
+      {
+        file: "images/recommend/recommend10.jpeg",
+        prodName:
+          "[쎈딜] 23FW 호주 어그 UGG 슬리퍼 /남녀 겨울 슬리퍼 양털 슬리퍼",
+        discount: 13,
+        price: 40940,
+        url: "j.html",
+      },
+      {
+        file: "images/recommend/recommend11.jpeg",
+        prodName:
+          "농심 컵라면 낱개6개 미니6입세트 육개장 김치사발면 짜파게티범벅 신라면 오징어짬뽕 새우탕 튀김우동",
+        discount: 14,
+        price: 8280,
+        url: "k.html",
+      },
+    ],
+    recommend_slide_best: [
+      {
+        file: "images/recommend/recommend1.jpeg",
+        prodName:
+          "[무료배송] GAP/네파키즈 유아 아동양말 특가전 6팩 건조기가능/KC인증",
+        discount: 50,
+        price: 6900,
+        url: "a.html",
+      },
+      {
+        file: "images/recommend/recommend1.jpeg",
+        prodName: "두번째json 2번째 항목입니다.",
+        discount: 50,
+        price: 6900,
+        url: "a.html",
+      },
+      {
+        file: "images/recommend/recommend1.jpeg",
+        prodName: "두번째json 3번째 항목입니다.",
+        discount: 50,
+        price: 6900,
+        url: "a.html",
+      },
+      {
+        file: "images/recommend/recommend1.jpeg",
+        prodName: "두번째json 4번째 항목입니다.",
+        discount: 50,
+        price: 6900,
+        url: "a.html",
+      },
+      {
+        file: "images/recommend/recommend5.jpeg",
+        prodName: "[스포츠파크] EXR 여성 보아 후리스 자켓 롱자켓 12종택1",
+        discount: 66,
+        price: 12900,
+        url: "e.html",
+      },
+      {
+        file: "images/recommend/recommend6.jpeg",
+        prodName:
+          "[만원의행복] 호두앤 토핑호두과자 오리지널 40알 외 앙버터/누텔라/블루베리 혼합 골라담기",
+        discount: 0,
+        price: 8730,
+        url: "f.html",
+      },
+      {
+        file: "images/recommend/recommend7.png",
+        prodName:
+          "[쎈딜] [I*POP] 아이팝 스파클링 워터 플레인 / 아이팝 탄산수 / 강탄산 / 진짜 먹는 샘물로 만든 탄산수",
+        discount: 22,
+        price: 6900,
+        url: "g.html",
+      },
+      {
+        file: "images/recommend/recommend8.jpeg",
+        prodName:
+          "[쎈딜][스포츠파크] 노스페이스 컴피 알파 플리스 집업 NJ4FP57 JKL ALL",
+        discount: 26,
+        price: 79000,
+        url: "h.html",
+      },
+      {
+        file: "images/recommend/recommend9.png",
+        prodName:
+          "[쎈딜] 야나두키즈 12개월 통합이용권+영어전집 (+30일 추가증정)",
+        discount: 88,
+        price: 160550,
+        url: "i.html",
+      },
+      {
+        file: "images/recommend/recommend10.jpeg",
+        prodName:
+          "[쎈딜] 23FW 호주 어그 UGG 슬리퍼 /남녀 겨울 슬리퍼 양털 슬리퍼",
+        discount: 13,
+        price: 40940,
+        url: "j.html",
+      },
+      {
+        file: "images/recommend/recommend11.jpeg",
+        prodName:
+          "농심 컵라면 낱개6개 미니6입세트 육개장 김치사발면 짜파게티범벅 신라면 오징어짬뽕 새우탕 튀김우동",
+        discount: 14,
+        price: 8280,
+        url: "k.html",
+      },
+    ],
+  };
+  res.send(result);
+});
 app.listen(port, () => {
   console.log(`현재 웹 서버가 포트번호 ${port} 으로 접속하였습니다.`);
 });
